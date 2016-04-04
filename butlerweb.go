5300dd02ce77e0edc76887a74e1756f7b86f3853
Amos Wenger <fasterthanlime@gmail.com>
2016-04-04 21:14:28 +0200

    Reading zip files is fun

--- butlerweb.go
+++ butlerweb.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
-	"os"
+	"time"
 
+	"github.com/dustin/go-humanize"
 	"github.com/flimzy/jsblob"
 	"github.com/gopherjs/gopherjs/js"
 )
@@ -58,81 +63,61 @@ func main() {
 	// log.Println("Patch generated!")
 }
 
-type html5File struct {
-	blob   jsblob.Blob
-	offset int64
-	size   int64
-	closed bool
-}
-
-var _ io.ReadCloser = (*html5File)(nil)
-var _ io.Seeker = (*html5File)(nil)
-
 // Diff runs some tests right now
 func Diff(file *js.Object) {
 	go func() {
 		log.Println("In diff!")
-		r := &html5File{
+		h5 := &html5File{
 			blob:   jsblob.Blob{*file},
 			offset: 0,
 			size:   file.Get("size").Int64(),
 			closed: false,
 		}
 
-		log.Println("Copying..")
-		io.Copy(os.Stdout, r)
-		log.Println("Done copying!")
+		t1 := time.Now()
+		filebytes := h5.blob.Bytes()
+
+		l := time.Since(t1)
+		log.Printf("Took %s to retrieve whole file", l.String())
+
+		r := bytes.NewReader(filebytes)
+		fullyReadZip(r, int64(len(filebytes)))
+
+		// fullyReadZip(h5, h5.size)
 	}()
 }
 
-func (h5 *html5File) Read(buf []byte) (n int, err error) {
-	log.Printf("Should read %d bytes\n", len(buf))
-
-	start := h5.offset
-	end := start + int64(len(buf))
-	if end > h5.size {
-		err = io.EOF
-		end = h5.size - 1
+func fullyReadZip(r io.ReaderAt, size int64) {
+	zr, err := zip.NewReader(r, size)
+	if err != nil {
+		panic(err)
 	}
 
-	log.Printf("Reading %d bytes\n", end-start)
+	var total int64
+	t1 := time.Now()
 
-	bs := h5.blob.Slice(int(start), int(end), "").Bytes()
-	copy(buf, bs)
+	for _, file := range zr.File {
+		log.Printf("%20s %s\n", humanize.Bytes(uint64(file.FileInfo().Size())), file.Name)
+		rc, err := file.Open()
+		if err != nil {
+			panic(err)
+		}
 
-	n = int(end - start)
-	if n > 0 {
-		err = nil
+		readBytes, err := io.Copy(ioutil.Discard, rc)
+		if err != nil {
+			panic(err)
+		}
+
+		total += readBytes
+
+		err = rc.Close()
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	h5.offset += (end - start)
-	return
-}
-
-func (h5 *html5File) Seek(whence int64, mode int) (n int64, err error) {
-	switch mode {
-	case os.SEEK_CUR:
-		h5.offset += whence
-	case os.SEEK_SET:
-		h5.offset = whence
-	case os.SEEK_END:
-		h5.offset = h5.size - whence
-	}
-
-	if h5.offset < 0 {
-		h5.offset = 0
-	}
-
-	if h5.offset >= h5.size {
-		h5.offset = h5.size
-	}
-
-	n = h5.offset
-
-	return
-}
-
-func (h5 *html5File) Close() (err error) {
-	h5.closed = true
-	return
+	len := time.Since(t1)
+	fmt.Printf("Total contents size: %s (read in %s, %s / s)\n",
+		humanize.Bytes(uint64(total)), len.String(),
+		humanize.Bytes(uint64(float64(total)/len.Seconds())))
 }
